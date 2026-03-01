14d1458e65578d92375b5a690a74fc4a1a4fb3d7
StefanPSchmid <schmiste@ethz.ch>
2026-03-01 15:53:56 +0100

    Update description

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
 	title: `Stefan P. Schmid`,
-	description: `Academic website of Stefan P. Schmid.`,
+	description: `Website of Stefan P. Schmid.`,
 	url: `https://www.stefanpschmid.com`, // No trailing slash!
 	name: `Stefan P. Schmid`,
 	enableThemeSwitcher: true,
